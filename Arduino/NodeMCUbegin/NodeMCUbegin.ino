18fc188da66a5320df8a2aab7232d62804c34d3e
SayuriYuto <dhuriamey02@gmail.com>
2022-10-14 00:11:23 +0530

    till date 13/10

--- Arduino/NodeMCUbegin/NodeMCUbegin.ino
+++ Arduino/NodeMCUbegin/NodeMCUbegin.ino
@@ -6,19 +6,79 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(smsbutton,INPUT);
   Serial.begin(115200);
-  Serial1.begin(115200);
+  Serial2.begin(115200);
+//  delay(10000);
+//  int inByte;
+//  if(Serial.available()){
+//    inByte = Serial.read();
+//    Serial.println("This is in Serial");
+//    Serial2.write(inByte);
+//  }
+//int inByte2;
+//  if(Serial2.available()){
+//    inByte2= Serial2.read();
+//    Serial.print("This is in Serial2");
+//    Serial.println(inByte);
+//  }
+//  char cstr[16];
+//  itoa(inByte,cstr,16);
+//  while (cstr != "OK")
+//  {    /* code */
+//  Serial.print(cstr);
+//  delay(1000);
+//  }
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  if(Serial.available()){
+    int inByte = Serial.read();
+    Serial.println("This is in Serial");
+    Serial2.write(inByte);
+    
+  }
+
+  if(Serial2.available()){
+    int inByte = Serial2.read();
+    Serial.println("This is in Serial2");
+    Serial.println(inByte);
+  }
+
   buttonState = digitalRead(smsbutton);
   if(buttonState!=lastButtonState){
     if(buttonState == HIGH){
       // do the thing
-      Serial
+      SendMessage();
 
     }
   }
   lastButtonState = buttonState;
   delay(1000);
+}
+
+void SendMessage(){
+  Serial.println("Setting the GSM in text mode");
+  Serial2.println("AT+CMGF=1\r");
+  Serial.println("AT+CMGF=1\r");
+
+  delay(2000);
+  Serial.println("Sending SMS to the desired phone number!");
+  Serial2.println("AT+CSMP=17,167,0,0\r");
+  Serial.println("AT+CSMP=17,167,0,0\r");
+
+  delay(2000);
+  Serial.println("Sending SMS to the desired phone number!");
+  Serial2.println("AT+CMGS=\"+917887575773\"\r");
+  Serial.println("AT+CMGS=\"+917887575773\"\r");
+
+  delay(2000);
+  Serial2.println("Hello from A9G");
+  Serial.println("Hello from serial");
+  delay(2000);
+
+  Serial2.println((char)26);
+  Serial.println((char)26);
+  // delay(10000);
 }
