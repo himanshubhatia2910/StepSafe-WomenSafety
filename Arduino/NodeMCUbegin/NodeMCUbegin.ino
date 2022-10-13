80b859398f6c5d3b253916dc34849c95f90f3efe
SayuriYuto <dhuriamey02@gmail.com>
2022-10-13 22:35:46 +0530

    Made updates to readme

--- Arduino/NodeMCUbegin/NodeMCUbegin.ino
+++ Arduino/NodeMCUbegin/NodeMCUbegin.ino
@@ -1,9 +1,24 @@
+#define smsbutton D5
+int buttonState = 0;
+int lastButtonState = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(smsbutton,INPUT);
+  Serial.begin(115200);
+  Serial1.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  buttonState = digitalRead(smsbutton);
+  if(buttonState!=lastButtonState){
+    if(buttonState == HIGH){
+      // do the thing
+      Serial
 
+    }
+  }
+  lastButtonState = buttonState;
+  delay(1000);
 }
