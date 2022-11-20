d0e62c371d4bf6c8d2012d1f7cbeaa156629de69
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2022-11-20 19:09:32 +0530

    Completed Till Login Page

--- app/src/main/java/com/example/stepsafe_womensafety/MainActivity.java
+++ app/src/main/java/com/example/stepsafe_womensafety/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.stepsafe_womensafety;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
