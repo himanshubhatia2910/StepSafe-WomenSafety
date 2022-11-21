b8c12474009052907c808fcf99e4ffc9ce82d845
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2022-11-21 19:52:23 +0530

    Home Page Added

--- app/src/main/java/com/example/stepsafe_womensafety/MainActivity.java
+++ app/src/main/java/com/example/stepsafe_womensafety/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(MainActivity.this,Information.class);
+                Intent intent=new Intent(MainActivity.this,Home.class);
                 startActivity(intent);
                 finish();
             }
