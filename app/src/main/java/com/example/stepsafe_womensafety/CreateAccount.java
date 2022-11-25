d5c663d968cbb487f99d188af82c09824fea9566
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2022-11-25 23:18:24 +0530

    Registration Page Updated

--- app/src/main/java/com/example/stepsafe_womensafety/CreateAccount.java
+++ app/src/main/java/com/example/stepsafe_womensafety/CreateAccount.java
@@ -14,6 +14,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 
 public class CreateAccount extends AppCompatActivity {
@@ -25,15 +30,15 @@ public class CreateAccount extends AppCompatActivity {
     FirebaseAuth mAuth;
     ImageView ShowHidePass;
 
-
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ShowHidePass = findViewById(R.id.show_pass_btn);
+
         super.onCreate(savedInstanceState);
 
 
         setContentView(R.layout.activity_create_account);
-
+        ShowHidePass = findViewById(R.id.show_pass_btn);
         etRegEmail = findViewById(R.id.etRegEmail);
         etRegPassword = findViewById(R.id.etRegPass);
         tvLoginHere = findViewById(R.id.SignIn);//not necesary
@@ -79,6 +84,29 @@ public class CreateAccount extends AppCompatActivity {
             mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                        assert firebaseUser != null;
+                        String userid = firebaseUser.getUid();
+                        reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+                        HashMap<String, String> hashMap = new HashMap<>();
+                        hashMap.put("id", userid);
+                        hashMap.put("email",email);
+                        hashMap.put("password",password);
+                        reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Intent intent = new Intent(CreateAccount.this, Information.class);
+                                    Toast.makeText(CreateAccount.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+                    }
+
                     if (task.isSuccessful()){
                         Toast.makeText(CreateAccount.this, "User registered successfully", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(CreateAccount.this, Information.class));
@@ -86,6 +114,7 @@ public class CreateAccount extends AppCompatActivity {
                         Toast.makeText(CreateAccount.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }
+
             });
         }
     }
--- app/src/main/java/com/example/stepsafe_womensafety/ui/Users.java
+++ app/src/main/java/com/example/stepsafe_womensafety/ui/Users.java
@@ -1,14 +1,15 @@
 package com.example.stepsafe_womensafety.ui;
 
 public class Users {
-    String email,name,blood_group,dob;
+    String email,name,blood_group,dob,password;
     int phone_number,age,height,weight,emergency_contact;
 
     public Users() {
     }
 
-    public Users(String email, String name, String blood_group, String dob, int phone_number, int age, int height, int weight, int emergency_contact) {
+    public Users(String email,String password, String name, String blood_group, String dob, int phone_number, int age, int height, int weight, int emergency_contact) {
         this.email = email;
+        this.password=password;
         this.name = name;
         this.blood_group = blood_group;
         this.dob = dob;
@@ -21,6 +22,9 @@ public class Users {
 
     public String getEmail() {
         return email;
+    }
+    public String getPassword() {
+        return password;
     }
 
     public String getName() {
