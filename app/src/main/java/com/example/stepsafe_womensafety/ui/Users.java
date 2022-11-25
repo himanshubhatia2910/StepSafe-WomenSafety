c71cab97e673e482598da71d236646566d91d121
Prajwal <prajwalkadam1172002@gmail.com>
2022-11-25 23:03:06 +0530

    Users adapter

---
+++ app/src/main/java/com/example/stepsafe_womensafety/ui/Users.java
@@ -0,0 +1,57 @@
+package com.example.stepsafe_womensafety.ui;
+
+public class Users {
+    String email,name,blood_group,dob;
+    int phone_number,age,height,weight,emergency_contact;
+
+    public Users() {
+    }
+
+    public Users(String email, String name, String blood_group, String dob, int phone_number, int age, int height, int weight, int emergency_contact) {
+        this.email = email;
+        this.name = name;
+        this.blood_group = blood_group;
+        this.dob = dob;
+        this.phone_number = phone_number;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.emergency_contact = emergency_contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBlood_group() {
+        return blood_group;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getEmergency_contact() {
+        return emergency_contact;
+    }
+}
