66d7b1d7a9bac666bbafb3045bd14d45dcb65926
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2022-11-26 22:14:53 +0530

    Photo Upload Partially Done

--- app/src/main/java/com/example/stepsafe_womensafety/Information.java
+++ app/src/main/java/com/example/stepsafe_womensafety/Information.java
@@ -3,27 +3,51 @@ package com.example.stepsafe_womensafety;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.stepsafe_womensafety.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.UUID;
 
 public class Information extends AppCompatActivity {
     EditText name,contact,blood,age,dob,weight,height,emergency;
     TextView save;
     FirebaseAuth mAuth;
     DatabaseReference reference;
+    ImageView IVPreviewImage;
+    int SELECT_PICTURE = 200;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
+    Uri selectedImageUri;
+    FirebaseStorage storage;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +61,19 @@ public class Information extends AppCompatActivity {
         weight = findViewById(R.id.weight);
         height = findViewById(R.id.height);
         emergency = findViewById(R.id.emergency);
+        IVPreviewImage = findViewById(R.id.photo);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        IVPreviewImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageChooser();
+            }
+
+
+
+
+        });
         save.setOnClickListener(view ->{
             String username = name.getText().toString();
             String phone = contact.getText().toString();
@@ -82,5 +119,84 @@ public class Information extends AppCompatActivity {
                 }
             });
         });
+    }
+    private void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                selectedImageUri = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                    IVPreviewImage.setImageBitmap(bitmap);
+                    uploadImage();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+//                if (null != selectedImageUri) {
+//                    // update the preview image in the layout
+//                    IVPreviewImage.setImageURI(selectedImageUri);
+
+            }
+        }
+    }
+
+    private void uploadImage() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading File....");
+        progressDialog.show();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+        StorageReference ref = storageReference.child("images/");
+
+
+        ref.putFile(selectedImageUri)
+//                .addOnSuccessListener(taskSnapshot -> {
+//
+//
+//                    //binding.firebaseimage.setImageURI(null);
+//                   IVPreviewImage.setImageURI(selectedImageUri);//testng
+//
+//                    Toast.makeText(Information.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+//                    if (progressDialog.isShowing())
+//                        progressDialog.dismiss();
+//
+//                })
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //IVPreviewImage.setImageURI(selectedImageUri);
+                        progressDialog.dismiss();
+                        Toast.makeText(Information.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> {
+
+
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+                    Toast.makeText(Information.this,"Failed to Upload",Toast.LENGTH_SHORT).show();
+
+
+                });
+
     }
 }
