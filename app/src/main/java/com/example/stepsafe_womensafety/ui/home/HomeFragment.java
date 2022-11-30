ed090ef3c0c297b082a22cab59fd9e4a987c858d
Himanshu Bhatia <himanshubhatia2910@gmail.com>
2022-11-30 15:09:11 +0530

    Home Page In Making

--- app/src/main/java/com/example/stepsafe_womensafety/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/stepsafe_womensafety/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.stepsafe_womensafety.ui.home;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,20 +14,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
-import com.example.stepsafe_womensafety.R;
 import com.example.stepsafe_womensafety.databinding.FragmentHomeBinding;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
