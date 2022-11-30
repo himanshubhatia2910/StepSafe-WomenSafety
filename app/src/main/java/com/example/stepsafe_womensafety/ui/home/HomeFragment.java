3a698856a1891e5be357d29faac8cb6010ac4e34
Prajwal <prajwalkadam1172002@gmail.com>
2022-11-30 15:19:40 +0530

    Working on fetching username

--- app/src/main/java/com/example/stepsafe_womensafety/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/stepsafe_womensafety/ui/home/HomeFragment.java
@@ -14,9 +14,21 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.stepsafe_womensafety.R;
 import com.example.stepsafe_womensafety.databinding.FragmentHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeFragment extends Fragment {
     ImageView rImage;
+    TextView username;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
 
     private FragmentHomeBinding binding;
 
@@ -25,12 +37,55 @@ public class HomeFragment extends Fragment {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
+
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        username = root.findViewById(R.id.name);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // below line is used to get
+        // reference for our database.
+        databaseReference = firebaseDatabase.getReference("Data");
+        getdata();
+
+
+
+
+
+
+
         return root;
 
 
 
+    }
+
+    private void getdata() {
+        // calling add value event listener method
+        // for getting the values from database.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // this method is call to get the realtime
+                // updates in the data.
+                // this method is called when the data is
+                // changed in our Firebase console.
+                // below line is for getting the data from
+                // snapshot of our database.
+                String value = snapshot.getValue(String.class);
+
+                // after getting the value we are setting
+                // our value to our text view in below line.
+                username.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // calling on cancelled method when we receive
+                // any error or we are not able to get the data.
+                Toast.makeText(getContext(), "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
